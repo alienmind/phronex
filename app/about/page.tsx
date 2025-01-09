4dcffd0f8e99d36fd9dfeb7b1bd50d84f58e8055
alienmind <alienmind@gmail.com>
2025-01-09 21:27:06 +0100

    Fixed buttons

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -34,17 +34,15 @@ function AboutPage() {
       <div className="flex relative px-6 md:px-24 flex-col items-center md:items-start justify-center min-h-[80dvh] h-full">
         <div className="flex flex-col items-center md:items-start justify-center gap-4">
           <p className="text-lg text-center md:text-left max-w-2xl">
-           This app has been done for learning purposes. The following resources are useful.
+           This app has been done for learning purposes.
           </p>
           <p className="text-lg text-center md:text-left max-w-2xl">
-            <ul>
-              <li>
-                <a href="https://github.com/alienmind/phronex">https://github.com/alienmind/auditorium</a><br/>
-              </li>
-            </ul>
+          <Button><Link href="https://github.com/alienmind/phronex">Visit github</Link></Button><br/>
           </p>
-          <p>
-          Additional debug actions that can be done from here:
+          <p className="text-lg text-center md:text-left max-w-2xl">
+          The following actions are dangerous, use with care!
+          </p>
+          <p className="text-lg text-center md:text-left max-w-2xl">
           <Button><Link href="/seed">Seed data</Link></Button>
           </p>
         <div className="flex flex-col items-center md:items-start justify-center gap-4">
