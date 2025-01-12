63540c92d5e92ec407b042bf2287d5c206cada00
alienmind <alienmind@gmail.com>
2025-01-12 11:53:47 +0100

    Extracted the slides content into a separate function for maintainability

--- src/app/about/slides/page.tsx
+++ src/app/about/slides/page.tsx
@@ -11,6 +11,25 @@ import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/black.css'; // "black" theme is just an example
 import { createPortal } from 'react-dom';
 
+
+/*
+ * This is the content of the slides
+ */
+function SlidesContent({deckDivRef, deckRef}: {deckDivRef: React.RefObject<HTMLDivElement>, deckRef: React.RefObject<Reveal.Api>}) {
+    return (
+        <div className="reveal" ref={deckDivRef}>
+            <div className="slides">
+                <section>Slide 1</section>
+                <section>Slide 2</section>
+            </div>
+        </div>
+    );
+}
+
+
+/*
+ * This is all of the boilerplate code for the slides being integrated in React
+ */
 export default function Slides() {
     const deckDivRef = useRef<HTMLDivElement>(null); // reference to deck container div
     const deckRef = useRef<Reveal.Api | null>(null); // reference to deck reveal instance
@@ -46,12 +65,7 @@ export default function Slides() {
       {createPortal(
         // Your presentation is sized based on the width and height of
         // our parent element. Make sure the parent is not 0-height.
-        <div className="reveal" ref={deckDivRef}>
-            <div className="slides">
-                <section>Slide 1</section>
-                <section>Slide 2</section>
-            </div>
-        </div>,
+        <SlidesContent {...{deckDivRef, deckRef}}/>,
         document.body
       )}
       </div>
